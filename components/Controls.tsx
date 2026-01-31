3333f57be0d2785966223fd14dcac5031ea0e7b0
Loopspeed Ltd <hi@loopspeed.co.uk>
2026-01-31 13:35:29 +0000

    Add pointer lock controls for desktop

---
+++ components/Controls.tsx
@@ -0,0 +1,19 @@
+import { PointerLockControls, OrbitControls } from '@react-three/drei'
+import { useState } from 'react'
+
+function isMobile() {
+  return /Mobi|Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent)
+}
+
+const MIN_POLAR_ANGLE = Math.PI / 2.75
+
+export default function Controls() {
+  const [mobile] = useState(isMobile())
+
+  return mobile ? (
+    <OrbitControls enablePan={false} enableZoom={false} enableRotate={true} minPolarAngle={MIN_POLAR_ANGLE} />
+  ) : (
+    // For pointer locked controls we need offset because they have different origin than OrbitControls
+    <PointerLockControls maxPolarAngle={Math.PI - MIN_POLAR_ANGLE} />
+  )
+}
--- components/Game.tsx
+++ components/Game.tsx
@@ -1,26 +1,20 @@
 'use client'
 
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
 import { type FC, Suspense } from 'react'
 import Environment from '@/components/Environment'
 
 import { Physics } from '@react-three/rapier'
 import Gun from '@/components/Gun'
 import Balloons from '@/components/Balloons'
+import Controls from '@/components/Controls'
 
 const Game: FC = () => {
   return (
     <Canvas camera={{ position: [0, 0, 10], fov: 30, near: 0.1, far: 500 }} shadows>
       <Suspense>
         <Environment />
-        <OrbitControls
-          enablePan={false}
-          enableZoom={false}
-          enableRotate={true}
-          minPolarAngle={Math.PI / 2.75}
-          maxPolarAngle={Math.PI}
-        />
+        <Controls />
 
         <Physics gravity={[0, 0, 0]}>
           <Balloons />
